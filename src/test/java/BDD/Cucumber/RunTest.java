f66a0bbfb103d56d3e3ec0798fe0ca1a5b51fe3d
jacob-zachariah <jacu.ker14@gmail.com>
2016-08-03 16:29:33 +0100

    dude this is working....>>

---
+++ src/test/java/BDD/Cucumber/RunTest.java
@@ -0,0 +1,11 @@
+package BDD.Cucumber;
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
+public class RunTest {
+
+}
